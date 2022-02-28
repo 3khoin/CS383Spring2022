bce06432b1bc37b333b9b76c0d10bc7f4b448a31
Chadwick <cbg1011oskar@gmail.com>
2022-02-28 10:22:32 -0800

    Remade and improved platformer level

---
+++ DungeonJump/Assets/ExitGate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if collide with player:
+        if( collision.gameObject.tag == "Player")
+        {
+            //change lvl to overworld:
+            SceneManager.LoadScene(0);
+        }
+    }
+}
