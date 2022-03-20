48ba512482564993e088b9d16716349cd2157ee0
Seth <sethcram@gmail.com>
2022-03-20 12:34:43 -0700

    Item interface implemented, mushroom biome removed

---
+++ DungeonJump/Assets/src/seth/My Scripts/Hazards/Hazard.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Hazard : MonoBehaviour
+{
+    public AudioSource hazardSound;
+    
+    virtual public void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("hazard trigger entered");
+    }
+    
+}
