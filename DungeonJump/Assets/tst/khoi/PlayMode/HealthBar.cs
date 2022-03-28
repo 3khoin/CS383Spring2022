d14508cbac91e4f1c242e05ac19ad180432937e0
3khoin <nbutrealking@gmail.com>
2022-03-28 00:51:32 -0700

    Added preliminary scripts + some audio. PREIVOUS VERSION WORKING

---
+++ DungeonJump/Assets/tst/khoi/PlayMode/HealthBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image fill;
+    public float playerHP;
+
+    private void Start()
+    {
+        fill = GameObject.Find("Fill").GetComponent<Image>();
+        playerHP = 1f;
+        fill.fillAmount = playerHP;
+    }
+
+    private void Update()
+    {
+        //fill.fillAmount = playerHP;
+        if (Time.timeScale == 1)
+        {
+            //if (playerHP <= 1f && playerHP > 0f) UpdateHP(-0.002f);
+            //if (playerHP == 0f) UpdateHP(1f);
+
+            // Check that player HP does not exceed bounds
+            if (playerHP < 0f) playerHP = 0f;
+            if (playerHP > 1f) playerHP = 1f;
+
+            fill.fillAmount = playerHP;
+        }
+    }
+
+    public void UpdateHP(float hpChange)
+    {
+        playerHP += hpChange;
+    }
+}
