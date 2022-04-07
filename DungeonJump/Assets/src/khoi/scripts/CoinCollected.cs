77e11f3ae582ea97dea0d363b5adec3b6d533a29
3khoin <nbutrealking@gmail.com>
2022-04-07 13:16:15 -0700

    Score + assembly definition file for Khoi

--- DungeonJump/Assets/src/khoi/scripts/CoinCollected.cs
+++ DungeonJump/Assets/src/khoi/scripts/CoinCollected.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinCollected : MonoBehaviour
+public class CoinCollected : PlatformerItem
 {
-    // Start is called before the first frame update
-    void Start()
+    override public void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            Pickup(null);     
+            PlayerManagerTmp.instance.UpdatePlayerScore(100);       
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    override public void Pickup(Collider2D other)
     {
-        
+        Destroy(gameObject);
     }
 }
