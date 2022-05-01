dc14ce24fba6ee25b2b3256e35c6278c77cd0d9a
Seth <sethcram@gmail.com>
2022-04-30 22:19:45 -0700

    Enemies added to platform levels.

--- DungeonJump/Assets/src/seth/MyScripts/EnemyMovement.cs
+++ DungeonJump/Assets/src/seth/MyScripts/EnemyMovement.cs
@@ -18,8 +18,9 @@ using UnityEngine;
  */
 
 public class EnemyMovement : MonoBehaviour {
-
+    
     public float speed;
+        // public float rotSpeed;
     public float followDistance;
 
     private Transform target; 
@@ -27,7 +28,10 @@ public class EnemyMovement : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
      
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();   
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        //check what dir to face every 0.5s
+        //InvokeRepeating("FaceDirMoving", 0.5f, 0.5f);
     }
 
     // Update is called once per frame
@@ -35,6 +39,49 @@ public class EnemyMovement : MonoBehaviour {
         
         if(Vector2.Distance(transform.position, target.position) > followDistance) {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            //rotate in direction traveling
+            /*
+            var targetPoint = new Vector3(target.position.x, transform.position.y, target.position.z);
+
+            //store direction calc'd from target + curr pos's (normalized so 0-1)
+            Vector3 dir = (targetPoint - transform.position).normalized;
+
+            //rot to look in dir
+            Quaternion lookRot = Quaternion.LookRotation(dir);
+
+            //apply rot to target dir
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRot, 360);
+            */
         }     
+    }
+
+    /*
+     * Assumes starting direction is facing left.
+     */
+    void FaceDirMoving()
+    {
+        /*
+        if (target.position.x < transform.position.x)
+        {
+            print("target x " + target.position.x + " is less than " + transform.position.x);
+
+            //face left
+            transform.eulerAngles = new Vector3(
+                transform.eulerAngles.x,
+                0,
+                transform.eulerAngles.y);
+        }
+        else if (target.position.x > transform.position.x)
+        {
+            print("target x " + target.position.x + " is greater than " + transform.position.x);
+
+            //face right
+            transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            180,
+            transform.eulerAngles.y);
+        }
+        */
     }
 }
