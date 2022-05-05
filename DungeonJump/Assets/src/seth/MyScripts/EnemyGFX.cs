4a50d6866ccc7c38623f599310fd2c5c86b43461
Seth <sethcram@gmail.com>
2022-05-04 21:02:45 -0700

    Enemy GFX updated and enemy movement enabled.

--- DungeonJump/Assets/src/seth/MyScripts/EnemyGFX.cs
+++ DungeonJump/Assets/src/seth/MyScripts/EnemyGFX.cs
@@ -7,6 +7,13 @@ public class EnemyGFX : MonoBehaviour
 {
     public AIPath aiPath;
 
+    private float startingXscale;
+
+    private void Start()
+    {
+        startingXscale = transform.localScale.x;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -16,7 +23,7 @@ public class EnemyGFX : MonoBehaviour
             print("Scale should be flipped");
 
             //flip over x-axis
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            transform.localScale = new Vector3(-startingXscale, transform.localScale.y, transform.localScale.z);
         }
         //if moving to left
         else if (aiPath.desiredVelocity.x <= -0.005f)
@@ -24,7 +31,7 @@ public class EnemyGFX : MonoBehaviour
             print("Scale should be flipped");
 
             //revert over x-axis
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(startingXscale, transform.localScale.y, transform.localScale.z);
         }
     }
 
